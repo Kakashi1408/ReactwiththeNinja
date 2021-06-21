d64e7efbe2e400517106afb28c7ddbf533bf7585
Kakashi <perocasithole124@gmail.com>
2021-06-22 01:45:40 +0200

    My_first_commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
+//This is not written in html but in jsx and then converted to html
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar></Navbar>
+      <div className = "content">
+        <Home />
+      </div>
     </div>
   );
 }
 
 export default App;
+//so that we can use it in other files
+
+
+
+/*THIS IS THE INITIAL LESSON CODE*/ 
+
+/*import './App.css';
+//This is not written in html but in jsx and then converted to html
+function App() {
+  //variable
+  const title = 'Welcome to the new blog';
+  const likes = 50;
+  const link = "http://www.google.com";
+
+  return (
+    <div className="App">
+      <div className = "content">
+        <h1>App Component</h1>
+        <h2>{ title }</h2>
+        <p>Liked { likes } times</p>
+        <p>{"Hello ninjas"}</p>
+
+        <a href={link}>Google site</a>
+      </div>
+    </div>
+  );
+}
+
+export default App;
+//so that we can use it in other files*/
